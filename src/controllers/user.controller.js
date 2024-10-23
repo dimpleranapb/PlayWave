bdbf607499cdd28ded90355e60924b75334b523f
Dimple Rana <dimpleranaji@gmail.com>
2024-10-23 16:05:54 +0530

    add watch history controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -356,6 +356,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       new ApiResponse(200, channel[0], "User channel fetched successfully")
     );
 });
+
+const getWatchHistory = asyncHandler(async () => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetch successfully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -367,4 +421,5 @@ export {
   updateUserCoverImage,
   changeCurrentPassword,
   getUserChannelProfile,
+  getWatchHistory,
 };
