dcffc4d578a987b39cdc1293bb124bee5e97e0cd
Dimple Rana <dimpleranaji@gmail.com>
2024-11-06 22:00:48 +0530

    Create and test getAllVideos Controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,15 +1,38 @@
 import mongoose, { isValidObjectId } from "mongoose";
-import Video  from "../models/video.model.js";
+import Video from "../models/video.model.js";
 import User from "../models/user.model.js";
-import  ApiError from "../utils/ApiError.js";
+import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 
-
 const getAllVideos = asyncHandler(async (req, res) => {
-  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-  //Get all videos based on query, sort, pagination
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.body;
+  //Filtering
+
+  let filters = {};
+  if (userId) filters.userId = userId;
+  if (query) filters.title = new RegExp(query, "i");
+
+
+  //Sorting
+  let sortOptions = {};
+  sortOptions[sortBy] = sortType === "asc" ? 1 : -1;
+
+  const skip = (page -1 )*10
+
+  const videos = await Video.find(filters)
+  .sort(sortOptions)
+  .skip(skip)
+  .limit(Number(limit))
+
+  if (videos.length === 0) {
+    throw new ApiError(404, "No videos found");
+  }
+
+  res.status(200)
+  .json(new ApiResponse(200, videos, "Videos data fetched successfully"))
+  
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -38,27 +61,22 @@ const publishAVideo = asyncHandler(async (req, res) => {
   if (!thumbnail) {
     throw new ApiError(400, "Thumbnail upload failed");
   }
-
   //Create Video
   const video = await Video.create({
-        videoFile: videoFile.url,
-        thumbnail: thumbnail.url,
-        title,
-        description,
-        duration: videoFile.duration,
-        owner: req.user._id
-
+    videoFile: videoFile.url,
+    thumbnail: thumbnail.url,
+    title,
+    description,
+    duration: videoFile.duration,
+    owner: req.user._id,
   });
-  
-  if(!video) {
+
+  if (!video) {
     throw new ApiError(400, "Fail to Publish Video");
   }
   return res
-  .status(200)
-  .json(new ApiResponse(200, video, "Video published successfully"))
-
+    .status(200)
+    .json(new ApiResponse(200, video, "Video published successfully"));
 });
 
-export{
-  publishAVideo, getAllVideos
-}
+export { publishAVideo, getAllVideos };
