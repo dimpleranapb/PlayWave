bc2bd7e2285e4ea15b61e3994454af0e82379c1d
Dimple Rana <dimpleranaji@gmail.com>
2024-11-13 19:44:13 +0530

    Start adding Subscription routes

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,31 @@
+import User from "../models/user.model"
+import mongoose, {isValidObjectId} from "mongoose"
+import Subscription from "../models/subscription.model"
+import ApiError from "../utils/ApiError"
+import ApiResponse from "../utils/ApiResponse"
+import asyncHandler from "../utils/asyncHandler"
+
+
+
+
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    // TODO: toggle subscription
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
