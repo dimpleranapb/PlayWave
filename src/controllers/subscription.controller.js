5f25250f7238da131722effe38a77d19920691ec
Dimple Rana <dimpleranaji@gmail.com>
2024-11-14 22:25:42 +0530

    add toggleSubscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -10,9 +10,36 @@ import asyncHandler from "../utils/asyncHandler"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+    const { channelId } = req.params;
+    const subscriber = req.user._id;
+
+    if (!channelId) {
+        throw new ApiError(400, "channelId is required");
+    }
+
+    // Check if a subscription already exists
+    const existingSubscription = await Subscription.findOne({
+        subscriber,
+        channel: channelId
+    });
+
+    if (existingSubscription) {
+        await existingSubscription.remove();
+        return res.status(200).json(new ApiResponse(200, "Channel Unsubscribed Successfully"));
+    } else {
+        const subscription = await Subscription.create({
+            subscriber,
+            channel: channelId
+        });
+
+        if (!subscription) {
+            throw new ApiError(400, "Something went wrong");
+        }
+
+        return res.status(200).json(new ApiResponse(200, "Channel Subscribed Successfully"));
+    }
+});
+
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
