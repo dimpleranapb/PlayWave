cb8932f45381f3b862914609087024de63b54c36
Dimple Rana <dimpleranaji@gmail.com>
2024-12-01 23:51:24 +0530

    Add getChannelStats Controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,7 +7,38 @@ import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const channelId = req.user._id;
+
+  // Get total subscribers
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: channelId,
+  });
+
+  // Get total views (assuming Video model has a views field)
+  const totalViews = await Video.aggregate([
+    { $match: { owner: channelId } },
+    { $group: { _id: null, totalViews: { $sum: "$views" } } },
+  ]);
+
+  const totalLikes = await Like.countDocuments({
+    video: { $in: await Video.find({ owner: channelId }).select("_id") },
+  });
+
+  // Get total videos
+  const totalVideos = await Video.countDocuments({ owner: channelId });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalSubscribers,
+        totalViews: totalViews[0] ? totalViews[0].totalViews : 0,
+        totalLikes,
+        totalVideos,
+      },
+      "Channel stats fetched successfully"
+    )
+  );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
